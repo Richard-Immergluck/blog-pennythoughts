725ed6f52b5d7a3d76fc3e8d40a68643fa32abc5
Richard Immergluck <rimmergluck@googlemail.com>
2021-01-28 14:01:44 +0000

    Initial commit and consolidation of W9 and W10 work

---
+++ python/models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from blog import db
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    image_file = db.Column(db.String(40), nullable=False, default='default.jpg')
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.date}', '{self.title}', '{self.content}')"
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    post = db.relationship('Post', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+class Todolist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content =  db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)    
+
---
+++ python/routes.py
@@ -0,0 +1,58 @@
+from blog.models import User, Post, Todolist
+from flask import render_template, url_for, request, redirect
+from blog import app, db
+
+@app.route('/')
+
+@app.route('/home')
+def home():
+    posts = Post.query.all()
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/todolist', methods=['POST','GET'])
+def todolist():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todolist(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('todolist')
+        except:
+            return 'There was an error'
+    else:
+        tasks = Todolist.query.order_by(Todolist.date_created).all()
+        return render_template('todolist.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+
+def delete(id):
+    task_to_delete = Todolist.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('todolist')
+    except:
+        return 'There was an error'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todolist.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('todolist')
+        except:
+            return 'Computer says no!'
+
+    else:
+        return render_template('update.html', task=task)
