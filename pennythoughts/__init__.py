dcbc9b368c10d0901c0ace9c9b6580e83142d6ff
Richard Immergluck <immergluckrj@cardiff.ac.uk>
2021-03-02 18:33:45 +0000

    Update __init__.py

--- pennythoughts/__init__.py
+++ pennythoughts/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_moment import Moment
+# from flask_moment import Moment
 # from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_mail import Mail
